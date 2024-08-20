931c0632d85224166592b9a93c67e67bb0676736
bishwajit_2810 <bishwajit2810@gmail.com>
2024-08-20 18:33:38 +0600

    maps

--- main.go
+++ main.go
@@ -1,26 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"learning_go/array"
-	"learning_go/conditionals"
-	"learning_go/functions"
-	"learning_go/loop"
-	"learning_go/myutill"
-	"learning_go/userinput"
-	"learning_go/variables"
+	maps_go "learning_go/maps"
 )
 
 func main() {
 
-	fmt.Println("hello world")
-	myutill.PrintMessage()
-	variables.Variable()
-	userinput.UserInput()
-	functions.Functions()
-	functions.ErrorHandling()
-	array.Array()
-	array.Slice()
-	conditionals.Conditional()
-	loop.Loop()
+	// fmt.Println("hello world")
+	// myutill.PrintMessage()
+	// variables.Variable()
+	// userinput.UserInput()
+	// functions.Functions()
+	// functions.ErrorHandling()
+	// array.Array()
+	// array.Slice()
+	// conditionals.Conditional()
+	// loop.Loop()
+	maps_go.MapsInGo()
 }
---
+++ maps/maps.go
@@ -0,0 +1,27 @@
+package maps_go
+
+import "fmt"
+
+func MapsInGo() {
+	// name <--> grade
+	bkGrade := make(map[string]float64)
+	bkGrade["bk 1st semester"] = 3.76
+	bkGrade["bk 2nd semester"] = 3.51
+	bkGrade["bk 3rd semester"] = 3.82
+	bkGrade["bk 4th semester"] = 3.57
+	bkGrade["bk 5th semester"] = 0.00
+
+	fmt.Println("semester--------grade")
+	for index, value := range bkGrade {
+		fmt.Println(index, value)
+	}
+	delete(bkGrade, "bk 5th semester")
+	fmt.Println("semester--------grade")
+	for index, value := range bkGrade {
+		fmt.Println(index, value)
+	}
+	semester, Exists := bkGrade["bk 4th semester"]
+	fmt.Println(semester, Exists)
+}
+
+// unordered data
